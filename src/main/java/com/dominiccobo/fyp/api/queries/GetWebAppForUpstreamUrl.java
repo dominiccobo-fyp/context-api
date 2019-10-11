9d34e93bb238a1fa76efcb251728948be255b17f
Dominic Cobo <dominiccobo@gmail.com>
2019-10-11 15:04:51 +0100

    Add missing files for previous commit

---
+++ src/main/java/com/dominiccobo/fyp/api/queries/GetWebAppForUpstreamUrl.java
@@ -0,0 +1,18 @@
+package com.dominiccobo.fyp.api.queries;
+
+public class GetWebAppForUpstreamUrl {
+
+    public final String upstreamUrl;
+
+    public GetWebAppForUpstreamUrl(String upstreamUrl) {
+        this.upstreamUrl = upstreamUrl;
+    }
+
+    public static class Result {
+        final String webAppUrl;
+
+        public Result(String webAppUrl) {
+            this.webAppUrl = webAppUrl;
+        }
+    }
+}
