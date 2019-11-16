0b704cdf3b0714e3c68c62f73504d1e670df4abe
Dominic Cobo <dominiccobo@gmail.com>
2019-11-16 15:18:05 +0000

    Add setters and builder to work item

--- src/main/java/com/dominiccobo/fyp/context/models/WorkItem.java
+++ src/main/java/com/dominiccobo/fyp/context/models/WorkItem.java
@@ -1,7 +1,27 @@
 package com.dominiccobo.fyp.context.models;
 
+import java.util.Optional;
+
 public class WorkItem {
 
     private String title;
     private String body;
+
+    public WorkItem setTitle(String title) {
+        this.title = title;
+        return this;
+    }
+
+    public Optional<String> getBody() {
+        return Optional.ofNullable(body);
+    }
+
+    public Optional<String> getTitle() {
+        return Optional.ofNullable(title);
+    }
+
+    public WorkItem setBody(String body) {
+        this.body = body;
+        return this;
+    }
 }
