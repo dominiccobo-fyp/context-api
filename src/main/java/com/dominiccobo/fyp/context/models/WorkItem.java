f961ccf7af2a1d8cfdf91ea056ff24944471485a
Dominic Cobo <dominiccobo@gmail.com>
2019-11-16 20:34:26 +0000

    Override equals for Workitems

--- src/main/java/com/dominiccobo/fyp/context/models/WorkItem.java
+++ src/main/java/com/dominiccobo/fyp/context/models/WorkItem.java
@@ -24,4 +24,22 @@ public class WorkItem {
         this.body = body;
         return this;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        WorkItem workItem = (WorkItem) o;
+
+        if (!title.equals(workItem.title)) return false;
+        return body.equals(workItem.body);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = title.hashCode();
+        result = 31 * result + body.hashCode();
+        return result;
+    }
 }
